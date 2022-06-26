51c0e65c70f92341221d92df0453b65b5b819fed
Darren Mulholland <dmulholl@tcd.ie>
2022-06-26 22:59:04 +0100

    Bump version to v0.7.5

--- src/vm/pyro.h
+++ src/vm/pyro.h
@@ -4,7 +4,7 @@
 // Language version.
 #define PYRO_VERSION_MAJOR 0
 #define PYRO_VERSION_MINOR 7
-#define PYRO_VERSION_PATCH 4
+#define PYRO_VERSION_PATCH 5
 
 // Pre-release label, e.g. "alpha1", "beta2", "rc3".
 #ifndef PYRO_VERSION_LABEL
