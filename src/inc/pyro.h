71b46d1d4026e777e31f44453cd5d7399e312771
Darren Mulholland <dmulholl@tcd.ie>
2022-12-18 17:32:39 +0100

    Housekeeping

--- src/inc/pyro.h
+++ src/inc/pyro.h
@@ -57,6 +57,8 @@
 #endif
 
 // Defining this macro causes header files to expose definitions conforming to POSIX 2008.
+// This macro must be defined *before* the standard library headers are imorted.
+// Ref: https://man7.org/linux/man-pages/man7/feature_test_macros.7.html
 #ifndef _XOPEN_SOURCE
     #define _XOPEN_SOURCE 700
 #endif
