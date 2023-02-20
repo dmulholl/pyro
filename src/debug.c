5deef5ad40a676dfa944b09285bb47629dc66642
Darren Mulholland <dmulholl@tcd.ie>
2023-02-20 23:05:09 +0000

    Housekeeping

--- src/debug.c
+++ src/debug.c
@@ -76,26 +76,37 @@ size_t pyro_disassemble_instruction(PyroVM* vm, PyroFn* fn, size_t ip) {
     switch (instruction) {
         case PYRO_OPCODE_ASSERT:
             return atomic_instruction(vm, "ASSERT", ip);
+
         case PYRO_OPCODE_BINARY_PLUS:
             return atomic_instruction(vm, "BINARY_PLUS", ip);
+
         case PYRO_OPCODE_BINARY_AMP:
             return atomic_instruction(vm, "BINARY_AMP", ip);
+
         case PYRO_OPCODE_UNARY_TILDE:
             return atomic_instruction(vm, "UNARY_TILDE", ip);
+
         case PYRO_OPCODE_BINARY_BAR:
             return atomic_instruction(vm, "BINARY_BAR", ip);
+
         case PYRO_OPCODE_BINARY_IN:
             return atomic_instruction(vm, "BINARY_IN", ip);
+
         case PYRO_OPCODE_BINARY_CARET:
             return atomic_instruction(vm, "BINARY_CARET", ip);
+
         case PYRO_OPCODE_CALL_VALUE:
             return u8_instruction(vm, "CALL_VALUE", fn, ip);
+
         case PYRO_OPCODE_CALL_VALUE_WITH_UNPACK:
             return u8_instruction(vm, "CALL_VALUE_WITH_UNPACK", fn, ip);
+
         case PYRO_OPCODE_MAKE_CLASS:
             return constant_instruction(vm, "MAKE_CLASS", fn, ip);
+
         case PYRO_OPCODE_CLOSE_UPVALUE:
             return atomic_instruction(vm, "CLOSE_UPVALUE", ip);
+
         case PYRO_OPCODE_MAKE_CLOSURE: {
             uint16_t const_index = (fn->code[ip + 1] << 8) | fn->code[ip + 2];
             ip += 3;
@@ -113,6 +124,7 @@ size_t pyro_disassemble_instruction(PyroVM* vm, PyroFn* fn, size_t ip) {
 
             return ip;
         }
+
         case PYRO_OPCODE_MAKE_CLOSURE_WITH_DEF_ARGS: {
             uint16_t const_index = (fn->code[ip + 1] << 8) | fn->code[ip + 2];
             ip += 4;
@@ -130,6 +142,7 @@ size_t pyro_disassemble_instruction(PyroVM* vm, PyroFn* fn, size_t ip) {
 
             return ip;
         }
+
         case PYRO_OPCODE_DEFINE_PRI_GLOBALS: {
             uint8_t count = fn->code[ip + 1];
             ip += 2;
@@ -146,6 +159,7 @@ size_t pyro_disassemble_instruction(PyroVM* vm, PyroFn* fn, size_t ip) {
 
             return ip;
         }
+
         case PYRO_OPCODE_DEFINE_PUB_GLOBALS: {
             uint8_t count = fn->code[ip + 1];
             ip += 2;
@@ -162,6 +176,7 @@ size_t pyro_disassemble_instruction(PyroVM* vm, PyroFn* fn, size_t ip) {
 
             return ip;
         }
+
         case PYRO_OPCODE_DEFINE_PRI_GLOBAL:
             return constant_instruction(vm, "DEFINE_PRI_GLOBAL", fn, ip);
 
@@ -185,182 +200,271 @@ size_t pyro_disassemble_instruction(PyroVM* vm, PyroFn* fn, size_t ip) {
 
         case PYRO_OPCODE_BINARY_EQUAL_EQUAL:
             return atomic_instruction(vm, "BINARY_EQUAL_EQUAL", ip);
+
         case PYRO_OPCODE_DEFINE_PRI_FIELD:
             return constant_instruction(vm, "DEFINE_PRI_FIELD", fn, ip);
+
         case PYRO_OPCODE_DEFINE_PUB_FIELD:
             return constant_instruction(vm, "DEFINE_PUB_FIELD", fn, ip);
+
         case PYRO_OPCODE_DEFINE_STATIC_FIELD:
             return constant_instruction(vm, "DEFINE_STATIC_FIELD", fn, ip);
+
         case PYRO_OPCODE_BINARY_SLASH:
             return atomic_instruction(vm, "BINARY_SLASH", ip);
+
         case PYRO_OPCODE_GET_FIELD:
             return constant_instruction(vm, "GET_FIELD", fn, ip);
+
         case PYRO_OPCODE_GET_PUB_FIELD:
             return constant_instruction(vm, "GET_PUB_FIELD", fn, ip);
+
         case PYRO_OPCODE_GET_GLOBAL:
             return constant_instruction(vm, "GET_GLOBAL", fn, ip);
+
         case PYRO_OPCODE_GET_INDEX:
             return atomic_instruction(vm, "GET_INDEX", ip);
+
         case PYRO_OPCODE_GET_LOCAL:
             return u8_instruction(vm, "GET_LOCAL", fn, ip);
+
         case PYRO_OPCODE_GET_MEMBER:
             return constant_instruction(vm, "GET_MEMBER", fn, ip);
+
         case PYRO_OPCODE_GET_METHOD:
             return constant_instruction(vm, "GET_METHOD", fn, ip);
+
         case PYRO_OPCODE_GET_PUB_METHOD:
             return constant_instruction(vm, "GET_PUB_METHOD", fn, ip);
+
         case PYRO_OPCODE_GET_SUPER_METHOD:
             return constant_instruction(vm, "GET_SUPER_METHOD", fn, ip);
+
         case PYRO_OPCODE_GET_UPVALUE:
             return u8_instruction(vm, "GET_UPVALUE", fn, ip);
+
         case PYRO_OPCODE_BINARY_GREATER:
             return atomic_instruction(vm, "BINARY_GREATER", ip);
+
         case PYRO_OPCODE_BINARY_GREATER_EQUAL:
             return atomic_instruction(vm, "BINARY_GREATER_EQUAL", ip);
+
         case PYRO_OPCODE_IMPORT_ALL_MEMBERS:
             return u8_instruction(vm, "IMPORT_ALL_MEMBERS", fn, ip);
+
         case PYRO_OPCODE_IMPORT_MODULE:
             return u8_instruction(vm, "IMPORT_MODULE", fn, ip);
+
         case PYRO_OPCODE_IMPORT_NAMED_MEMBERS:
             return u8_x2_instruction(vm, "IMPORT_NAMED_MEMBERS", fn, ip);
+
         case PYRO_OPCODE_INHERIT:
             return atomic_instruction(vm, "INHERIT", ip);
+
         case PYRO_OPCODE_CALL_METHOD:
             return invoke_instruction(vm, "CALL_METHOD", fn, ip);
+
         case PYRO_OPCODE_CALL_PUB_METHOD:
             return invoke_instruction(vm, "CALL_PUB_METHOD", fn, ip);
+
         case PYRO_OPCODE_CALL_METHOD_WITH_UNPACK:
             return invoke_instruction(vm, "CALL_METHOD_WITH_UNPACK", fn, ip);
+
         case PYRO_OPCODE_CALL_PUB_METHOD_WITH_UNPACK:
             return invoke_instruction(vm, "CALL_PUB_METHOD_WITH_UNPACK", fn, ip);
+
         case PYRO_OPCODE_CALL_SUPER_METHOD:
             return invoke_instruction(vm, "CALL_SUPER_METHOD", fn, ip);
+
         case PYRO_OPCODE_CALL_SUPER_METHOD_WITH_UNPACK:
             return invoke_instruction(vm, "CALL_SUPER_METHOD_WITH_UNPACK", fn, ip);
+
         case PYRO_OPCODE_GET_ITERATOR:
             return atomic_instruction(vm, "GET_ITERATOR", ip);
+
         case PYRO_OPCODE_GET_NEXT_FROM_ITERATOR:
             return atomic_instruction(vm, "GET_NEXT_FROM_ITERATOR", ip);
+
         case PYRO_OPCODE_JUMP:
             return jump_instruction(vm, "JUMP", 1, fn, ip);
+
         case PYRO_OPCODE_JUMP_IF_ERR:
             return jump_instruction(vm, "JUMP_IF_ERR", 1, fn, ip);
+
         case PYRO_OPCODE_JUMP_IF_FALSE:
             return jump_instruction(vm, "JUMP_IF_FALSE", 1, fn, ip);
+
         case PYRO_OPCODE_JUMP_IF_NOT_ERR:
             return jump_instruction(vm, "JUMP_IF_NOT_ERR", 1, fn, ip);
+
         case PYRO_OPCODE_JUMP_IF_NOT_KINDA_FALSEY:
             return jump_instruction(vm, "JUMP_IF_NOT_KINDA_FALSEY", 1, fn, ip);
+
         case PYRO_OPCODE_JUMP_IF_NOT_NULL:
             return jump_instruction(vm, "JUMP_IF_NOT_NULL", 1, fn, ip);
+
         case PYRO_OPCODE_JUMP_IF_TRUE:
             return jump_instruction(vm, "JUMP_IF_TRUE", 1, fn, ip);
+
         case PYRO_OPCODE_BINARY_LESS:
             return atomic_instruction(vm, "BINARY_LESS", ip);
+
         case PYRO_OPCODE_BINARY_LESS_EQUAL:
             return atomic_instruction(vm, "BINARY_LESS_EQUAL", ip);
+
         case PYRO_OPCODE_LOAD_CONSTANT:
             return constant_instruction(vm, "LOAD_CONSTANT", fn, ip);
+
         case PYRO_OPCODE_LOAD_FALSE:
             return atomic_instruction(vm, "LOAD_FALSE", ip);
+
         case PYRO_OPCODE_LOAD_I64_0:
             return atomic_instruction(vm, "LOAD_I64_0", ip);
+
         case PYRO_OPCODE_LOAD_I64_1:
             return atomic_instruction(vm, "LOAD_I64_1", ip);
+
         case PYRO_OPCODE_LOAD_I64_2:
             return atomic_instruction(vm, "LOAD_I64_2", ip);
+
         case PYRO_OPCODE_LOAD_I64_3:
             return atomic_instruction(vm, "LOAD_I64_3", ip);
+
         case PYRO_OPCODE_LOAD_I64_4:
             return atomic_instruction(vm, "LOAD_I64_4", ip);
+
         case PYRO_OPCODE_LOAD_I64_5:
             return atomic_instruction(vm, "LOAD_I64_5", ip);
+
         case PYRO_OPCODE_LOAD_I64_6:
             return atomic_instruction(vm, "LOAD_I64_6", ip);
+
         case PYRO_OPCODE_LOAD_I64_7:
             return atomic_instruction(vm, "LOAD_I64_7", ip);
+
         case PYRO_OPCODE_LOAD_I64_8:
             return atomic_instruction(vm, "LOAD_I64_8", ip);
+
         case PYRO_OPCODE_LOAD_I64_9:
             return atomic_instruction(vm, "LOAD_I64_9", ip);
+
         case PYRO_OPCODE_GET_LOCAL_0:
             return atomic_instruction(vm, "GET_LOCAL_0", ip);
+
         case PYRO_OPCODE_GET_LOCAL_1:
             return atomic_instruction(vm, "GET_LOCAL_1", ip);
+
         case PYRO_OPCODE_GET_LOCAL_2:
             return atomic_instruction(vm, "GET_LOCAL_2", ip);
+
         case PYRO_OPCODE_GET_LOCAL_3:
             return atomic_instruction(vm, "GET_LOCAL_3", ip);
+
         case PYRO_OPCODE_GET_LOCAL_4:
             return atomic_instruction(vm, "GET_LOCAL_4", ip);
+
         case PYRO_OPCODE_GET_LOCAL_5:
             return atomic_instruction(vm, "GET_LOCAL_5", ip);
+
         case PYRO_OPCODE_GET_LOCAL_6:
             return atomic_instruction(vm, "GET_LOCAL_6", ip);
+
         case PYRO_OPCODE_GET_LOCAL_7:
             return atomic_instruction(vm, "GET_LOCAL_7", ip);
+
         case PYRO_OPCODE_GET_LOCAL_8:
             return atomic_instruction(vm, "GET_LOCAL_8", ip);
+
         case PYRO_OPCODE_GET_LOCAL_9:
             return atomic_instruction(vm, "GET_LOCAL_9", ip);
+
         case PYRO_OPCODE_SET_LOCAL_0:
             return atomic_instruction(vm, "SET_LOCAL_0", ip);
+
         case PYRO_OPCODE_SET_LOCAL_1:
             return atomic_instruction(vm, "SET_LOCAL_1", ip);
+
         case PYRO_OPCODE_SET_LOCAL_2:
             return atomic_instruction(vm, "SET_LOCAL_2", ip);
+
         case PYRO_OPCODE_SET_LOCAL_3:
             return atomic_instruction(vm, "SET_LOCAL_3", ip);
+
         case PYRO_OPCODE_SET_LOCAL_4:
             return atomic_instruction(vm, "SET_LOCAL_4", ip);
+
         case PYRO_OPCODE_SET_LOCAL_5:
             return atomic_instruction(vm, "SET_LOCAL_5", ip);
+
         case PYRO_OPCODE_SET_LOCAL_6:
             return atomic_instruction(vm, "SET_LOCAL_6", ip);
+
         case PYRO_OPCODE_SET_LOCAL_7:
             return atomic_instruction(vm, "SET_LOCAL_7", ip);
+
         case PYRO_OPCODE_SET_LOCAL_8:
             return atomic_instruction(vm, "SET_LOCAL_8", ip);
+
         case PYRO_OPCODE_SET_LOCAL_9:
             return atomic_instruction(vm, "SET_LOCAL_9", ip);
+
         case PYRO_OPCODE_LOAD_NULL:
             return atomic_instruction(vm, "LOAD_NULL", ip);
+
         case PYRO_OPCODE_LOAD_TRUE:
             return atomic_instruction(vm, "LOAD_TRUE", ip);
+
         case PYRO_OPCODE_JUMP_BACK:
             return jump_instruction(vm, "JUMP_BACK", -1, fn, ip);
+
         case PYRO_OPCODE_BINARY_LESS_LESS:
             return atomic_instruction(vm, "BINARY_LESS_LESS", ip);
+
         case PYRO_OPCODE_MAKE_MAP:
             return u16_instruction(vm, "MAKE_MAP", fn, ip);
+
         case PYRO_OPCODE_MAKE_SET:
             return u16_instruction(vm, "MAKE_SET", fn, ip);
+
         case PYRO_OPCODE_MAKE_VEC:
             return u16_instruction(vm, "MAKE_VEC", fn, ip);
+
         case PYRO_OPCODE_DEFINE_PRI_METHOD:
             return constant_instruction(vm, "DEFINE_PRI_METHOD", fn, ip);
+
         case PYRO_OPCODE_DEFINE_PUB_METHOD:
             return constant_instruction(vm, "DEFINE_PUB_METHOD", fn, ip);
+
         case PYRO_OPCODE_DEFINE_STATIC_METHOD:
             return constant_instruction(vm, "DEFINE_STATIC_METHOD", fn, ip);
+
         case PYRO_OPCODE_BINARY_PERCENT:
             return atomic_instruction(vm, "BINARY_PERCENT", ip);
+
         case PYRO_OPCODE_BINARY_STAR:
             return atomic_instruction(vm, "BINARY_STAR", ip);
+
         case PYRO_OPCODE_UNARY_MINUS:
             return atomic_instruction(vm, "UNARY_MINUS", ip);
+
         case PYRO_OPCODE_UNARY_PLUS:
             return atomic_instruction(vm, "UNARY_PLUS", ip);
+
         case PYRO_OPCODE_UNARY_BANG:
             return atomic_instruction(vm, "UNARY_BANG", ip);
+
         case PYRO_OPCODE_BINARY_BANG_EQUAL:
             return atomic_instruction(vm, "BINARY_BANG_EQUAL", ip);
+
         case PYRO_OPCODE_POP:
             return atomic_instruction(vm, "POP", ip);
+
         case PYRO_OPCODE_POP_ECHO_IN_REPL:
             return atomic_instruction(vm, "POP_ECHO_IN_REPL", ip);
+
         case PYRO_OPCODE_POP_JUMP_IF_FALSE:
             return jump_instruction(vm, "POP_JUMP_IF_FALSE", 1, fn, ip);
+
         case PYRO_OPCODE_BINARY_STAR_STAR:
             return atomic_instruction(vm, "BINARY_STAR_STAR", ip);
 
@@ -372,68 +476,100 @@ size_t pyro_disassemble_instruction(PyroVM* vm, PyroFn* fn, size_t ip) {
 
         case PYRO_OPCODE_BINARY_GREATER_GREATER:
             return atomic_instruction(vm, "BINARY_GREATER_GREATER", ip);
+
         case PYRO_OPCODE_SET_FIELD:
             return constant_instruction(vm, "SET_FIELD", fn, ip);
+
         case PYRO_OPCODE_SET_PUB_FIELD:
             return constant_instruction(vm, "SET_PUB_FIELD", fn, ip);
+
         case PYRO_OPCODE_SET_GLOBAL:
             return constant_instruction(vm, "SET_GLOBAL", fn, ip);
+
         case PYRO_OPCODE_SET_INDEX:
             return atomic_instruction(vm, "SET_INDEX", ip);
+
         case PYRO_OPCODE_SET_LOCAL:
             return u8_instruction(vm, "SET_LOCAL", fn, ip);
+
         case PYRO_OPCODE_SET_UPVALUE:
             return u8_instruction(vm, "SET_UPVALUE", fn, ip);
+
         case PYRO_OPCODE_BINARY_MINUS:
             return atomic_instruction(vm, "BINARY_MINUS", ip);
+
         case PYRO_OPCODE_BINARY_SLASH_SLASH:
             return atomic_instruction(vm, "BINARY_SLASH_SLASH", ip);
+
         case PYRO_OPCODE_TRY:
             return atomic_instruction(vm, "TRY", ip);
+
         case PYRO_OPCODE_UNPACK:
             return u8_instruction(vm, "UNPACK", fn, ip);
+
         case PYRO_OPCODE_START_WITH:
             return atomic_instruction(vm, "START_WITH", ip);
+
         case PYRO_OPCODE_END_WITH:
             return atomic_instruction(vm, "END_WITH", ip);
+
         case PYRO_OPCODE_LOAD_CONSTANT_0:
             return atomic_instruction(vm, "LOAD_CONSTANT_0", ip);
+
         case PYRO_OPCODE_LOAD_CONSTANT_1:
             return atomic_instruction(vm, "LOAD_CONSTANT_1", ip);
+
         case PYRO_OPCODE_LOAD_CONSTANT_2:
             return atomic_instruction(vm, "LOAD_CONSTANT_2", ip);
+
         case PYRO_OPCODE_LOAD_CONSTANT_3:
             return atomic_instruction(vm, "LOAD_CONSTANT_3", ip);
+
         case PYRO_OPCODE_LOAD_CONSTANT_4:
             return atomic_instruction(vm, "LOAD_CONSTANT_4", ip);
+
         case PYRO_OPCODE_LOAD_CONSTANT_5:
             return atomic_instruction(vm, "LOAD_CONSTANT_5", ip);
+
         case PYRO_OPCODE_LOAD_CONSTANT_6:
             return atomic_instruction(vm, "LOAD_CONSTANT_6", ip);
+
         case PYRO_OPCODE_LOAD_CONSTANT_7:
             return atomic_instruction(vm, "LOAD_CONSTANT_7", ip);
+
         case PYRO_OPCODE_LOAD_CONSTANT_8:
             return atomic_instruction(vm, "LOAD_CONSTANT_8", ip);
+
         case PYRO_OPCODE_LOAD_CONSTANT_9:
             return atomic_instruction(vm, "LOAD_CONSTANT_9", ip);
+
         case PYRO_OPCODE_CALL_VALUE_0:
             return atomic_instruction(vm, "CALL_VALUE_0", ip);
+
         case PYRO_OPCODE_CALL_VALUE_1:
             return atomic_instruction(vm, "CALL_VALUE_1", ip);
+
         case PYRO_OPCODE_CALL_VALUE_2:
             return atomic_instruction(vm, "CALL_VALUE_2", ip);
+
         case PYRO_OPCODE_CALL_VALUE_3:
             return atomic_instruction(vm, "CALL_VALUE_3", ip);
+
         case PYRO_OPCODE_CALL_VALUE_4:
             return atomic_instruction(vm, "CALL_VALUE_4", ip);
+
         case PYRO_OPCODE_CALL_VALUE_5:
             return atomic_instruction(vm, "CALL_VALUE_5", ip);
+
         case PYRO_OPCODE_CALL_VALUE_6:
             return atomic_instruction(vm, "CALL_VALUE_6", ip);
+
         case PYRO_OPCODE_CALL_VALUE_7:
             return atomic_instruction(vm, "CALL_VALUE_7", ip);
+
         case PYRO_OPCODE_CALL_VALUE_8:
             return atomic_instruction(vm, "CALL_VALUE_8", ip);
+
         case PYRO_OPCODE_CALL_VALUE_9:
             return atomic_instruction(vm, "CALL_VALUE_9", ip);
 
