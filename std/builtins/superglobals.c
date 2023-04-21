f14c36fde3adbd756c7c501172a6d677f9288af8
Darren Mulholland <dmulholl@tcd.ie>
2023-04-21 21:17:36 +0100

    Add support for supplying a module argument to $exec()

--- std/builtins/superglobals.c
+++ std/builtins/superglobals.c
@@ -739,16 +739,30 @@ static PyroValue fn_input(PyroVM* vm, size_t arg_count, PyroValue* args) {
 
 
 static PyroValue fn_exec(PyroVM* vm, size_t arg_count, PyroValue* args) {
+    if (arg_count == 0 || arg_count > 2) {
+        pyro_panic(vm, "$exec(): expected 1 or 2 arguments, found %zu", arg_count);
+        return pyro_null();
+    }
+
     if (!PYRO_IS_STR(args[0])) {
         pyro_panic(vm, "$exec(): invalid argument [code], expected a string");
         return pyro_null();
     }
     PyroStr* code = PYRO_AS_STR(args[0]);
 
-    PyroMod* module = PyroMod_new(vm);
-    if (!module) {
-        pyro_panic(vm, "$exec(): out of memory");
-        return pyro_null();
+    PyroMod* module;
+    if (arg_count == 2) {
+        if (!PYRO_IS_MOD(args[1])) {
+            pyro_panic(vm, "$exec(): invalid argument [module], expected a module");
+            return pyro_null();
+        }
+        module = PYRO_AS_MOD(args[1]);
+    } else {
+        module = PyroMod_new(vm);
+        if (!module) {
+            pyro_panic(vm, "$exec(): out of memory");
+            return pyro_null();
+        }
     }
 
     // Push the module onto the stack to keep it safe from the garbage collector.
@@ -947,7 +961,7 @@ void pyro_load_std_builtins(PyroVM* vm) {
     pyro_define_global_fn(vm, "$env", fn_env, -1);
     pyro_define_global_fn(vm, "$eprint", fn_eprint, -1);
     pyro_define_global_fn(vm, "$eprintln", fn_eprintln, -1);
-    pyro_define_global_fn(vm, "$exec", fn_exec, 1);
+    pyro_define_global_fn(vm, "$exec", fn_exec, -1);
     pyro_define_global_fn(vm, "$exit", fn_exit, 1);
     pyro_define_global_fn(vm, "$f64", fn_f64, 1);
     pyro_define_global_fn(vm, "$field", fn_field, 2);
