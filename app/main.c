767fca9714c6a036a99986fc82a46d7660226818
Darren Mulholland <dmulholl@tcd.ie>
2023-05-27 15:17:03 +0100

    Fix use after free bug for exit code

--- app/main.c
+++ app/main.c
@@ -14,18 +14,23 @@ int main(int argc, char* argv[]) {
     const unsigned char* code;
     size_t code_length;
 
-    if (pyro_get_embedded("main.pyro", &code, &code_length)) {
-        pyro_exec_code(vm, (const char*)code, code_length, "main.pyro", NULL);
-        if (pyro_get_exit_flag(vm) || pyro_get_panic_flag(vm)) {
-            pyro_free_vm(vm);
-            exit(pyro_get_exit_code(vm));
-        }
+    if (!pyro_get_embedded("main.pyro", &code, &code_length)) {
+        fprintf(stderr, "error: missing 'main.pyro' file\n");
+        exit(1);
+    }
 
-        pyro_run_main_func(vm);
-        if (pyro_get_exit_flag(vm) || pyro_get_panic_flag(vm)) {
-            pyro_free_vm(vm);
-            exit(pyro_get_exit_code(vm));
-        }
+    pyro_exec_code(vm, (const char*)code, code_length, "main.pyro", NULL);
+    if (pyro_get_exit_flag(vm) || pyro_get_panic_flag(vm)) {
+        int64_t exit_code = pyro_get_exit_code(vm);
+        pyro_free_vm(vm);
+        exit(exit_code);
+    }
+
+    pyro_run_main_func(vm);
+    if (pyro_get_exit_flag(vm) || pyro_get_panic_flag(vm)) {
+        int64_t exit_code = pyro_get_exit_code(vm);
+        pyro_free_vm(vm);
+        exit(exit_code);
     }
 
     pyro_free_vm(vm);
