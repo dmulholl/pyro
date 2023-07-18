377c725fbe3ea7fff403c83627985188388e5a31
Darren Mulholland <dmulholl@tcd.ie>
2023-07-18 10:25:45 +0100

    Housekeeping

--- inc/compiler.h
+++ inc/compiler.h
@@ -1,8 +1,12 @@
 #ifndef pyro_compiler_h
 #define pyro_compiler_h
 
-// This function will panic and return NULL if a syntax error occurs or if memory allocation
-// fails.
+// This function compiles source code into bytecode. It will panic and return NULL if the code
+// contains a syntax error or if memory allocation fails.
+// - [src_code] does not require a terminating-null.
+// - [src_len] is the number of bytes in [src_code], excluding any terminating null.
+// - [src_id] is used to identify the source code in error messages (e.g. its file path).
+// - [dump_bytecode] is a debugging flag for dumping the compiled bytecode to stdout.
 PyroFn* pyro_compile(PyroVM* vm, const char* src_code, size_t src_len, const char* src_id, bool dump_bytecode);
 
 #endif
