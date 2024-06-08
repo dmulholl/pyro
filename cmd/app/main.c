6bd218aad4f5647802ceaa1125e5be12990e41d3
Darren Mulholland <dmulholl@tcd.ie>
2024-06-08 10:48:59 +0100

    Fix entrypoint for baked application binaries

--- cmd/app/main.c
+++ cmd/app/main.c
@@ -13,22 +13,19 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
-    const unsigned char* code;
-    size_t code_length;
-
-    if (!pyro_find_embedded_file("main.pyro", &code, &code_length)) {
+    PyroBuf* code = pyro_load_embedded_file(vm, "main.pyro");
+    if (!code) {
         fprintf(stderr, "error: missing 'embed/main.pyro' file\n");
+        pyro_free_vm(vm);
         exit(1);
     }
 
-    pyro_exec_code(vm, (const char*)code, code_length, "main.pyro", NULL);
-    if (pyro_get_exit_flag(vm) || pyro_get_panic_flag(vm)) {
-        int64_t exit_code = pyro_get_exit_code(vm);
+    if (!pyro_push(vm, pyro_obj(code))) {
         pyro_free_vm(vm);
-        exit(exit_code);
+        exit(1);
     }
 
-    pyro_run_main_func(vm);
+    pyro_exec_code(vm, (const char*)code->bytes, code->count, "main.pyro", NULL);
     if (pyro_get_exit_flag(vm) || pyro_get_panic_flag(vm)) {
         int64_t exit_code = pyro_get_exit_code(vm);
         pyro_free_vm(vm);
