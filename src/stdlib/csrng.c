6ba7a3563bef02e73806ef50907f9840934d19f2
Darren Mulholland <dmulholl@tcd.ie>
2024-07-28 19:26:02 +0100

    Add std::csrng::rand_i64()

--- src/stdlib/csrng.c
+++ src/stdlib/csrng.c
@@ -16,6 +16,18 @@ static PyroValue fn_rand_bytes(PyroVM* vm, size_t arg_count, PyroValue* args) {
 }
 
 
+static PyroValue fn_rand_i64(PyroVM* vm, size_t arg_count, PyroValue* args) {
+    PyroBuf* buf = pyro_csrng_rand_bytes(vm, 8, "rand_bytes()");
+    if (vm->halt_flag) {
+        return pyro_null();
+    }
+
+    int64_t result = *((int64_t*)buf->bytes);
+    return pyro_i64(result);
+}
+
+
 void pyro_load_stdlib_module_csrng(PyroVM* vm, PyroMod* module) {
     pyro_define_pub_member_fn(vm, module, "rand_bytes", fn_rand_bytes, 1);
+    pyro_define_pub_member_fn(vm, module, "rand_i64", fn_rand_i64, 0);
 }
